74a9a639e458701b0b68bb53b4f93130d4967574
Ename45 <enamesit@gmail.com>
2023-06-20 22:24:28 +0530

    second commit

---
+++ client/src/views/components/feed/feeds.jsx
@@ -0,0 +1,48 @@
+import person2 from '../../../assets/images/svg/person2.svg'
+import person3 from '../../../assets/images/svg/person3.svg'
+import person4 from '../../../assets/images/svg/person4.svg'
+import productDesigner from '../../../assets/images/svg/productDesigner.svg'
+import softwareDeveloper from '../../../assets/images/svg/softwareDeveloper.svg'
+import politicalAnalyst from '../../../assets/images/svg/politicalAnalyst.svg'
+
+export const feeds = [
+  {
+    profileImage: person2,
+    fullName: "Grace Ikpang",
+    role: "Product designer",
+    datePublished: "May 25th, 2023",
+    title: "Starting out as a Product designer",
+    minutesRead: "10 mins read",
+    post: "Embarking on a journey as a product designer can be an exhilarating and fulfilling experience. As a profession that bridges the realms of art, technology, and problem-solving, product design offers an opportunity to shape the way people interact with the world around them.",
+    anyImage: productDesigner,
+    comment: 200,
+    likes: 120,
+    views: "2980 views",
+  },
+  {
+    profileImage: person4,
+    fullName: "Oluwatobi Ogunniyi",
+    role: "Software developer",
+    datePublished: ".May 22nd, 2023",
+    title: "Using CSS Selectors",
+    minutesRead: "10 mins read",
+    post: "Cascading Style Sheets (CSS) is a fundamental technology that lies at the heart of modern web design. It empowers developers and designers to transform plain HTML into visually captivating and interactive web pages",
+    anyImage: softwareDeveloper,
+    comment: 200,
+    likes: 120,
+    views: "2980 views",
+  },
+  {
+    profileImage: person3,
+    fullName: "Adebobola Muhydeen",
+    role: "Political analyst",
+    datePublished: ".May 23rd, 2023",
+    title: "The Impact of social media on Political discourse",
+    minutesRead: "10 mins read",
+    post: "In the era of digital communication, social media platforms have become pervasive in our daily lives, revolutionizing the way we connect, share information, and engage with the world",
+    anyImage: politicalAnalyst,
+    comment: 200,
+    likes: 120,
+    views: "2980 views",
+  },
+]
