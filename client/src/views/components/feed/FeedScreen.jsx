93eca4b69b595d926f97e2300fea2210afe92c33
Ename45 <enamesit@gmail.com>
2023-06-20 20:57:33 +0530

    first commit

--- client/src/views/components/feed/FeedScreen.jsx
+++ client/src/views/components/feed/FeedScreen.jsx
@@ -6,10 +6,13 @@ const FeedScreen = () => {
   return (
     <div className="feed-main-container">
       <aside>
-
+        <h1>ASIDE</h1>
       </aside>
+      <header>
+        <h1>HEADER</h1>
+      </header>
       <section>
-
+      <h1>SECTION</h1>
       </section>
     </div>
   )
